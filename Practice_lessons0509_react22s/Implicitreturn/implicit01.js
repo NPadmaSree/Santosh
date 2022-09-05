1bdde7e75c39fc3ad10d5c403732d0c1f68370b7
Padma.nandini <padmasree.nandini@gmail.com>
2022-09-05 20:10:48 +0300

    implicit function and few more strings

---
+++ Practice_lessons0509_react22s/Implicitreturn/implicit01.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+Re-write the function triple without using the keyword return.
+
+
+/**
+ * @param {number} value
+ */
+const triple = (value) => value * 3;
+
+// Sample usage - do not modify
+console.log(triple(2)); // 6
+console.log(triple(3)); // 9
+
+/* 
+const getMessage=(message)=>{
+    if(message.endWith('.')){
+        return message;
+    }else{
+        return message?'.';
+    }
+};
+ */
